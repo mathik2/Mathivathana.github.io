083a33234b63cd4b1857388b2f73c6a3ebea35b9
Mathivathana <45286002+mathik2@users.noreply.github.com>
2022-01-11 12:07:26 +0530

    added some functionality

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-var profileVisit = document.getElementById("VisitCount")
-function countVisit(){
+setInterval(heartbeat,1000);
+
+
+function heartbeat(){
+    let heartColor = document.getElementById('heart').style.color;
+    if(heartColor=='red'){
+        document.getElementById('heart').style.color="pink";
+        
+       
+    }
+   else {
+    document.getElementById('heart').style.color="red";
     
+   
+   }
 }
+
+
+
+( function () {
+    let d = new Date().getHours();
+    
+console.log(new Date().getHours() );
+    if (d >= 5 & d < 12) {
+        document.getElementById('wish').innerText = "Good morning :) Guest!!";
+    } else if (d >= 12 & d < 15) {
+        document.getElementById('wish').innerText = "Good afternoon :) Guest!!";
+    }
+    else if (d >= 15 & d < 18) {
+        document.getElementById('wish').innerText = "Good evening :) Guest!!";
+    }
+    else if (d >= 18 & d < 24) {
+        document.getElementById('wish').innerText = "Good night :) Guest!!";
+    }
+})();
+setTimeout(function () { document.getElementById('greeting').remove(); }, 8000);
